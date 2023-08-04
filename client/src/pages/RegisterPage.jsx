0a393d5c5432e377b674756b41056e65b373bdd8
Yeison Gil <yeisongil.a9701@gmail.com>
2023-08-04 08:57:41 -0500

    Inicio de creacion del componente RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,20 @@
+import { useForm } from 'react-hook-form';
+
+
 
 function RegisterPage() {
+
+    // const { }
+
   return (
-    <div>RegisterPage</div>
+    <div>
+        <form action="">
+            <input type="text" name="" id="" />
+            <input type="email" name="" id="" />
+            <input type="password" name="" id="" />
+            <button type="submit" className="rounded-full">Registrar</button>
+        </form>
+    </div>
   )
 }
 
