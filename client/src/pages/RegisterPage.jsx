dad0fbe67792d26b4c5d52bc81e953ec1a5a24d0
Yeison Gil <yeisongil.a9701@gmail.com>
2023-08-04 11:48:10 -0500

    Terminado el registro del front

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,21 +1,25 @@
 import { useForm } from 'react-hook-form';
-
+import { registerRequest } from '../api/auth';
 
 
 function RegisterPage() {
 
-    // const { }
+    const { register, handleSubmit } =useForm();
 
   return (
-    <div>
-        <form action="">
-            <input type="text" name="" id="" />
-            <input type="email" name="" id="" />
-            <input type="password" name="" id="" />
-            <button type="submit" className="rounded-full">Registrar</button>
+    <div className='bg-blue-950 max-w-md p-10 rounded-md'>
+        <form onSubmit={handleSubmit(async (values) => {
+          console.log(values);
+          const res = await registerRequest(values)
+          console.log(res);
+        })}>
+            <input type="text" {...register("username", {required:true})} placeholder='username' className='w-full bg-gray-50 text-black px-4 py-4 rounded-md my-2' />
+            <input type="email" {...register("email", {required:true})} placeholder='email'  className='w-full bg-gray-50 text-black px-4 py-4 rounded-md my-2'/>
+            <input type="password" {...register("password", {required:true})} placeholder='password' className='w-full bg-gray-50 text-black px-4 py-4 rounded-md my-2' />
+            <button type="submit" className="rounded bg-blue-400 text-white px-4 my-2 py-2">Registrar</button>
         </form>
     </div>
   )
 }
 
-export default RegisterPage
+export default RegisterPage;
